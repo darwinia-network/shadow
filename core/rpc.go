f35e60ddac6d8c083f1917ab0b9229c93e4a9ec8
clearloop <udtrokia@gmail.com>
2020-04-27 02:06:18 +0800

    add: json-rpc library

---
+++ core/rpc.go
@@ -0,0 +1,73 @@
+package core
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"net/http"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+// rpcRequest represents a RPC request.
+// rpcRequest implements the io.ReadWriteCloser interface.
+type rpcRequest struct {
+	r    io.Reader     // holds the JSON formated RPC request
+	rw   io.ReadWriter // holds the JSON formated RPC response
+	done chan bool     // signals then end of the RPC request
+}
+
+// NewRPCRequest returns a new rpcRequest.
+func newRPCRequest(r io.Reader) *rpcRequest {
+	var buf bytes.Buffer
+	done := make(chan bool)
+	return &rpcRequest{r, &buf, done}
+}
+
+// Read implements the io.ReadWriteCloser Read method.
+func (r *rpcRequest) Read(p []byte) (n int, err error) {
+	return r.r.Read(p)
+}
+
+// Write implements the io.ReadWriteCloser Write method.
+func (r *rpcRequest) Write(p []byte) (n int, err error) {
+	return r.rw.Write(p)
+}
+
+// Close implements the io.ReadWriteCloser Close method.
+func (r *rpcRequest) Close() error {
+	r.done <- true
+	return nil
+}
+
+// Call invokes the RPC request, waits for it to complete, and returns the results.
+func (r *rpcRequest) Call() io.Reader {
+	go jsonrpc.ServeConn(r)
+	<-r.done
+	return r.rw
+}
+
+// Serve rpc methods with port
+func Serve(methods interface{}, port string) error {
+	err := rpc.Register(methods)
+	if err != nil {
+		return errors.New("Generate RPC methods failed")
+	}
+
+	// Gather all requests to `/`
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		defer req.Body.Close()
+		w.Header().Set("Content-Type", "application/json")
+
+		res := newRPCRequest(req.Body).Call()
+		_, err = io.Copy(w, res)
+	})
+
+	// Check the err in callback
+	if err != nil {
+		return errors.New("Generate response failed")
+	}
+
+	// Return the err to caller
+	return http.ListenAndServe(port, nil)
+}
--- go.mod
+++ go.mod
@@ -4,17 +4,17 @@ go 1.14
 
 require (
 	github.com/ethereum/go-ethereum v1.8.22
-	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.4.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/onsi/ginkgo v1.11.0 // indirect
 	github.com/onsi/gomega v1.8.1 // indirect
 	github.com/stretchr/testify v1.5.1 // indirect
 	github.com/tranvictor/ethashproof v0.0.0-20200216154736-82a2b716eac4
-	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
-	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd // indirect
+	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd // indirect
+	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
 	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )

eef84bcdedc8c00c000b13e9b8c14ea9c2db99d4
clearloop <udtrokia@gmail.com>
2020-08-26 16:57:11 +0800

    fix(examples): block async functions

--- examples/header.rs
+++ examples/header.rs
@@ -1,8 +1,10 @@
 use shadow::chain::eth::{EthHeader, EthHeaderRPCResp};
 
 fn main() {
-    let client = reqwest::blocking::Client::new();
-    let header = EthHeaderRPCResp::get(&client, 1).unwrap().result;
+    let client = reqwest::Client::new();
+    let header = async_std::task::block_on(EthHeaderRPCResp::get(&client, 1))
+        .unwrap()
+        .result;
     println!("{:?}", &header);
     println!("{:?}", i64::from_str_radix("1f", 16));
     println!("{:?}", Into::<EthHeader>::into(header));
--- examples/runner.rs
+++ examples/runner.rs
@@ -4,5 +4,5 @@ fn main() {
     env_logger::init();
     let conn = shadow::pool::conn(None);
     let mut runner = Runner::with(conn);
-    runner.start().unwrap();
+    async_std::task::block_on(runner.start()).unwrap();
 }

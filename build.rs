1f4237ea17b9a6e624b21185f7ffe296840229cd
clearloop <udtrokia@gmail.com>
2020-08-29 12:14:59 +0800

    fix(ci): force link-search out_dir while building static library

--- build.rs
+++ build.rs
@@ -6,7 +6,9 @@ fn main() {
     println!("cargo:rerun-if-changed=path/to/Cargo.lock");
 
     // Declare build args
-    let staticlib = format!("{}/libdarwinia_shadow.a", env::var("OUT_DIR").unwrap());
+    let mut dynamic = true;
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let staticlib = format!("{}/libdarwinia_shadow.a", out_dir);
     let dylib = format!(
         "/usr/local/lib/libdarwinia_shadow.{}",
         match String::from_utf8_lossy(Command::new("uname").output().unwrap().stdout.as_slice())
@@ -36,14 +38,22 @@ fn main() {
 
     // Build the link library
     if !Command::new("go")
-        .args(&args(true))
+        .args(&args(dynamic))
         .status()
         .unwrap()
         .success()
     {
-        Command::new("go").args(&args(false)).status().unwrap();
+        dynamic = false;
+        Command::new("go").args(&args(dynamic)).status().unwrap();
+        println!("built static library at {}", out_dir);
     }
 
     // Post-check
-    println!("cargo:rustc-link-lib=darwinia_shadow");
+    if dynamic {
+        println!("cargo:rustc-link-search=/usr/local/lib");
+        println!("cargo:rustc-link-lib=dylib=darwinia_shadow");
+    } else {
+        println!("cargo:rustc-link-search={}", out_dir);
+        println!("cargo:rustc-link-lib=static=darwinia_shadow");
+    }
 }

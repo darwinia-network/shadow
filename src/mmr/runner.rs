1f198e792b6e7825f5ea024c9f59652fa4462e23
Aki <wuminzhe@gmail.com>
2020-09-05 09:52:16 +0800

    Remove mmr_size calc of every mmr push

--- src/mmr/runner.rs
+++ src/mmr/runner.rs
@@ -35,33 +35,36 @@ impl From<ShadowShared> for Runner {
 impl Runner {
     /// Start the runner
     pub async fn start(&mut self) -> Result<(), Error> {
-        let mut ptr = {
-            let last_leaf =
-                helper::mmr_size_to_last_leaf(self.db.iterator(IteratorMode::Start).count() as i64);
+        let mut mmr_size = self.db.iterator(IteratorMode::Start).count() as u64;
+        let last_leaf = helper::mmr_size_to_last_leaf(mmr_size as i64);
+        let mut ptr =
             if last_leaf == 0 {
                 0
             } else {
                 last_leaf + 1
-            }
-        };
+            };
 
         loop {
-            if let Err(e) = self.push(ptr).await {
-                trace!("Push block to mmr_store failed: {:?}", e);
-                trace!("MMR service restarting after 10s...");
-                actix_rt::time::delay_for(time::Duration::from_secs(10)).await;
-            } else {
-                if ptr
-                    % env::var("MMR_LOG")
+            match self.push(ptr, mmr_size).await {
+                Err(e) => {
+                    trace!("Push block to mmr_store failed: {:?}", e);
+                    trace!("MMR service restarting after 10s...");
+                    actix_rt::time::delay_for(time::Duration::from_secs(10)).await;
+                },
+                Ok(mmr_size_new) => {
+                    if ptr
+                        % env::var("MMR_LOG")
                         .unwrap_or_else(|_| "10000".to_string())
                         .parse::<i64>()
                         .unwrap_or(10000)
-                    == 0
-                {
-                    trace!("Pushed mmr {} into database", ptr);
-                }
+                        == 0
+                    {
+                        trace!("Pushed mmr {} into database", ptr);
+                    }
 
-                ptr += 1;
+                    mmr_size = mmr_size_new;
+                    ptr += 1;
+                }
             }
         }
     }
@@ -75,12 +78,7 @@ impl Runner {
     }
 
     /// Push new header hash into storage
-    pub async fn push(&mut self, number: i64) -> Result<(), Error> {
-        let mmr_size = if number == 0 {
-            0
-        } else {
-            cmmr::leaf_index_to_mmr_size((number - 1) as u64)
-        } as u64;
+    pub async fn push(&mut self, number: i64, mmr_size: u64) -> Result<u64, Error> {
         let mut mmr = MMR::<_, MergeHash, _>::new(mmr_size, &self.store);
         mmr.push(H256::from(
             &EthHeaderRPCResp::get(&self.client, number as u64)
@@ -88,16 +86,17 @@ impl Runner {
                 .result
                 .hash,
         ))?;
+        let mmr_size_new = mmr.mmr_size();
 
         mmr.commit()?;
-        Ok(())
+        Ok(mmr_size_new)
     }
 
     /// Gen mmrs for tests
     pub async fn stops_at(&mut self, count: i64) -> Result<(), Error> {
-        let mmr_size = self.db.iterator(IteratorMode::Start).count() as i64;
+        let mut mmr_size = self.db.iterator(IteratorMode::Start).count() as u64;
         let mut ptr = {
-            let last_leaf = helper::mmr_size_to_last_leaf(mmr_size);
+            let last_leaf = helper::mmr_size_to_last_leaf(mmr_size as i64);
             if last_leaf == 0 {
                 0
             } else {
@@ -109,8 +108,10 @@ impl Runner {
             if ptr >= count {
                 break;
             }
-            self.push(ptr).await?;
-            ptr += 1;
+            if let Ok(mmr_size_new) = self.push(ptr, mmr_size).await {
+                mmr_size = mmr_size_new;
+                ptr += 1;
+            }
         }
 
         Ok(())

153155144eb8fe22936c5bfcdaa1b15fc515b49c
Aki <wuminzhe@gmail.com>
2020-09-08 10:59:58 +0800

    Add 'confirmations' to response of eth/header api

--- src/api/eth/header.rs
+++ src/api/eth/header.rs
@@ -5,11 +5,18 @@ use crate::{
 };
 use actix_web::{web, Responder};
 use cmmr::MMR;
+use std::env;
 
 #[derive(Serialize)]
 struct ProofResp {
     header: EthHeaderJson,
     mmr_root: String,
+}
+
+#[derive(Serialize)]
+struct Resp {
+	proof: ProofResp,
+	confirmations: u64,
 }
 
 /// Proof target header
@@ -32,12 +39,37 @@ pub async fn handle(block: web::Path<String>, shared: web::Data<ShadowShared>) -
                 .unwrap_or_default(),
         )
     };
+    
+	let proof = ProofResp {
+		header: EthHeader::get(&shared.client, num)
+		.await
+		.unwrap_or_default()
+		.into(),
+		mmr_root: format!("0x{}", root),
+	};
 
-    web::Json(ProofResp {
-        header: EthHeader::get(&shared.client, num)
-            .await
-            .unwrap_or_default()
-            .into(),
-        mmr_root: format!("0x{}", root),
-    })
+	let confirmations = get_confirmations(num).await.unwrap_or(0);
+
+	web::Json(Resp {
+		proof: proof,
+		confirmations: confirmations
+	})
+}
+
+pub async fn get_confirmations(block_number: u64) -> web3::Result<u64> {
+
+	let rpc_url = env::var("ETHEREUM_RPC").unwrap_or_else(|_| {
+		if env::var("ETHEREUM_ROPSTEN").is_ok() {
+			crate::conf::DEFAULT_ETHEREUM_ROPSTEN_RPC.into()
+		} else {
+			crate::conf::DEFAULT_ETHEREUM_RPC.into()
+		}
+	});
+
+	let transport = web3::transports::Http::new(&rpc_url)?;
+	let web3 = web3::Web3::new(transport);
+
+	let current_height = web3.eth().block_number().await?;
+
+	Ok(current_height.as_u64() - block_number)
 }

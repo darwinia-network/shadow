2437a809453a44c3b4a526076a655df6ada173b3
clearloop <udtrokia@gmail.com>
2020-10-30 18:38:16 +0800

    perf(rpcs): prints ethreum rpcs in init log

--- src/cmd/run.rs
+++ src/cmd/run.rs
@@ -2,9 +2,6 @@ use crate::{api, mmr::Runner, result::Error, ShadowShared};
 
 /// Run shadow service
 pub async fn exec(port: u16, verbose: bool) -> Result<(), Error> {
-    let shared = ShadowShared::new(None);
-    let mut runner = Runner::from(shared.clone());
-
     if std::env::var("RUST_LOG").is_err() {
         if verbose {
             std::env::set_var("RUST_LOG", "info,darwinia_shadow");
@@ -13,6 +10,9 @@ pub async fn exec(port: u16, verbose: bool) -> Result<(), Error> {
         }
     }
     env_logger::init();
+
+    let shared = ShadowShared::new(None);
+    let mut runner = Runner::from(shared.clone());
     let (mr, ar) = futures::join!(api::serve(port, shared), runner.start());
     mr?;
     ar?;
--- src/shared.rs
+++ src/shared.rs
@@ -19,20 +19,20 @@ pub struct ShadowShared {
 }
 
 fn ethereum_rpc(http: Client) -> EthereumRPC {
-    EthereumRPC::new(
-        http,
-        env::var("ETHEREUM_RPC")
-            .unwrap_or_else(|_| {
-                if env::var("ETHEREUM_ROPSTEN").is_ok() {
-                    crate::conf::DEFAULT_ETHEREUM_ROPSTEN_RPC.into()
-                } else {
-                    crate::conf::DEFAULT_ETHEREUM_RPC.into()
-                }
-            })
-            .split(',')
-            .map(|s| s.to_string())
-            .collect(),
-    )
+    let rpcs = env::var("ETHEREUM_RPC")
+        .unwrap_or_else(|_| {
+            if env::var("ETHEREUM_ROPSTEN").is_ok() {
+                crate::conf::DEFAULT_ETHEREUM_ROPSTEN_RPC.into()
+            } else {
+                crate::conf::DEFAULT_ETHEREUM_RPC.into()
+            }
+        })
+        .split(',')
+        .map(|s| s.to_string())
+        .collect();
+
+    info!("Avaiable ethereum rpc urls: \n{:#?}", rpcs);
+    EthereumRPC::new(http, rpcs)
 }
 
 impl ShadowShared {

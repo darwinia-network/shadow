a8265b6ece8a73871ac73ef2aed9eb18396857f5
HackFisher <hackfisher@gmail.com>
2020-11-02 16:57:04 +0800

    mmr leaf patch for master

--- src/api/ethereum/mod.rs
+++ src/api/ethereum/mod.rs
@@ -2,6 +2,7 @@
 mod count;
 mod ffi;
 mod mmr_root;
+mod mmr_leaf;
 mod parcel;
 mod proof;
 mod receipt;
@@ -10,6 +11,7 @@ pub use self::{
     count::handle as count,
     ffi::{epoch, import},
     mmr_root::handle as mmr_root,
+    mmr_leaf::handle as mmr_leaf,
     parcel::handle as parcel,
     proof::{handle as proof, ProposalReq},
     receipt::handle as receipt,

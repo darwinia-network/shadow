a09a1620a49d448d13b34b8f1f76b2de7b9e7fdc
HackFisher <hackfisher@gmail.com>
2020-11-03 21:45:33 +0800

    comment out due to #101

--- src/mmr/runner.rs
+++ src/mmr/runner.rs
@@ -49,14 +49,14 @@ impl Runner {
         let mut ptr = if last_leaf == 0 { 0 } else { last_leaf + 1 };
 
         // Using a cache rpc block number to optimize and reduce rpc call.
-        let mut last_rpc_block_number = self.0.eth.block_number().await?;
+        // let mut last_rpc_block_number = self.0.eth.block_number().await?;
 
         loop {
-            if last_rpc_block_number < (ptr as u64 + 12) {
-                last_rpc_block_number = self.0.eth.block_number().await?;
-                actix_rt::time::delay_for(time::Duration::from_secs(10)).await;
-                continue;
-            }
+            // if last_rpc_block_number < (ptr as u64 + 12) {
+            //     last_rpc_block_number = self.0.eth.block_number().await?;
+            //     actix_rt::time::delay_for(time::Duration::from_secs(10)).await;
+            //     continue;
+            // }
 
             // Note:
             //

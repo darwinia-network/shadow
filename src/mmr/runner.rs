d5c17babdb0331372779a19b49cf69c72f62aaab
xiaocheng <xiaoch2010@gmail.com>
2020-11-06 17:38:15 +0800

    stop launch if ethereum network changed

--- src/mmr/runner.rs
+++ src/mmr/runner.rs
@@ -1,11 +1,12 @@
 //! MMR Runner
 use crate::{
     api::ethereum::epoch,
-    mmr::{hash::MergeHash, helper},
+    mmr::{hash::MergeHash, helper, H256},
     result::Error,
     ShadowShared,
 };
 use cmmr::MMR;
+use cmmr::{MMRStore};
 use primitives::rpc::RPC;
 use rocksdb::IteratorMode;
 use std::{env, thread, time};
@@ -41,10 +42,35 @@ impl Runner {
         }
     }
 
+    /// translate from hash to network name
+    pub fn network_name(h: &str) -> &str {
+        match h {
+            "41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d" => "Ropsten",
+            "d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3" => "Mainnet",
+            "6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177" => "Rinkeby",
+            "bf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a" => "Goerli",
+            _ => "unknown",
+        }
+    }
+
     /// Start the runner
     pub async fn start(&mut self) -> Result<(), Error> {
         // MMR variables
         let mut mmr_size = self.as_mut().db.iterator(IteratorMode::Start).count() as u64;
+        info!("last mmr size {}", mmr_size);
+
+        if mmr_size > 0 {
+            let valid_hash = H256::hex(&(&self.as_ref().store).get_elem(0).unwrap().unwrap_or([0;32]));
+            let hash_from_ethereum = self.0.eth.get_header_by_number(0).await?.hash;
+
+            if let Some(hash) = hash_from_ethereum {
+                let rpc_hash = H256::hex(&hash);
+                assert!(valid_hash == rpc_hash, "RPC network should be {} but {}", Runner::network_name(&valid_hash), Runner::network_name(&rpc_hash));
+            } else {
+                panic!("rpc request is unreachable");
+            }
+        }
+
         let last_leaf = helper::mmr_size_to_last_leaf(mmr_size as i64);
         let mut ptr = if last_leaf == 0 { 0 } else { last_leaf + 1 };
 

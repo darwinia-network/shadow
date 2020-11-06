3347a312359173c7cd6549d2a42df15fd8cfdb36
clearloop <udtrokia@gmail.com>
2020-11-07 00:51:07 +0800

    feat(err_resp): add error resp to mmr_root

--- src/api/ethereum/mmr_root.rs
+++ src/api/ethereum/mmr_root.rs
@@ -3,7 +3,7 @@ use crate::{
     mmr::{MergeHash, H256},
     ShadowShared,
 };
-use actix_web::{web, Responder};
+use actix_web::{error, web, Responder};
 use cmmr::MMR;
 use primitives::chain::ethereum::MMRRootJson;
 
@@ -19,17 +19,20 @@ use primitives::chain::ethereum::MMRRootJson;
 #[allow(clippy::eval_order_dependence)]
 pub async fn handle(block: web::Path<String>, shared: web::Data<ShadowShared>) -> impl Responder {
     let num: u64 = block.to_string().parse().unwrap_or(0);
-    let root = if num == 0 {
-        "0000000000000000000000000000000000000000000000000000000000000000".to_string()
-    } else {
-        H256::hex(
-            &MMR::<_, MergeHash, _>::new(cmmr::leaf_index_to_mmr_size(num - 1), &shared.store)
-                .get_root()
-                .unwrap_or_default(),
-        )
-    };
+    if num == 0 {
+        return Err(error::ErrorBadRequest("Requesting mmr of block 0"));
+    }
 
-    web::Json(MMRRootJson {
-        mmr_root: format!("0x{}", root),
-    })
+    if let Ok(hash_bytes) =
+        MMR::<_, MergeHash, _>::new(cmmr::leaf_index_to_mmr_size(num - 1), &shared.store).get_root()
+    {
+        Ok(web::Json(MMRRootJson {
+            mmr_root: format!("0x{}", H256::hex(&hash_bytes)),
+        }))
+    } else {
+        Err(error::ErrorInternalServerError(format!(
+            "Get mmr root of block {} failed",
+            num
+        )))
+    }
 }

8a1ca6817a6c0c4a85f5204738e106c66417ab68
xiaoch05 <xiaoch2010@gmail.com>
2020-11-13 11:31:46 +0800

    Remove redundancy code in golang-pkg (#108) (#112)
    * Remove redundancy code in golang-pkg (#108)
    
    * rmv DEFAULT_ETHEREUM_RPC

--- pkg/shadow/config.go
+++ pkg/shadow/config.go
@@ -3,18 +3,7 @@ package shadow
 import (
 	"os"
 	"path/filepath"
-	"strconv"
-
 	"github.com/darwinia-network/shadow/pkg/shadow/util"
-)
-
-const (
-	ETHEREUM_ROPSTEN     string = "ETHEREUM_ROPSTEN"
-	ETHEREUM_RPC         string = "ETHEREUM_RPC"
-	SHADOW_GENESIS       string = "SHADOW_GENESIS"
-	GETH_DATADIR         string = "GETH_DATADIR"
-	DEFAULT_ETHEREUM_RPC string = "https://mainnet.infura.io/v3/0bfb9acbb13c426097aabb1d81a9d016"
-	DEFAULT_ROPSTEN_RPC  string = "https://ropsten.infura.io/v3/0bfb9acbb13c426097aabb1d81a9d016"
 )
 
 type RawConfig struct {
@@ -22,8 +11,6 @@ type RawConfig struct {
 }
 
 type Config struct {
-	Api     string `json:"api"`
-	Genesis uint64 `json:"genesis"`
 	Root    string `json:"root"`
 }
 
@@ -34,28 +21,6 @@ func (c *Config) Load() error {
 	c.Root, err = RootDir()
 	if err != nil {
 		return err
-	}
-
-	// Load infura key
-	gen := os.Getenv(SHADOW_GENESIS)
-	if gen == "" {
-		gen = "0"
-	}
-
-	// Construct shadow genesis
-	c.Genesis, err = strconv.ParseUint(gen, 10, 64)
-	if err != nil {
-		return err
-	}
-
-	// Load api from env
-	c.Api = os.Getenv(ETHEREUM_RPC)
-	if c.Api == "" {
-		if os.Getenv(ETHEREUM_ROPSTEN) == "" {
-			c.Api = DEFAULT_ETHEREUM_RPC
-		} else {
-			c.Api = DEFAULT_ROPSTEN_RPC
-		}
 	}
 
 	return nil

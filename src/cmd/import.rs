580a31e0951357c7465b9d34b49fa9dabce8c2b7
Aki <wuminzhe@gmail.com>
2020-11-30 23:19:19 +0800

    Init logger for import

--- src/cmd/import.rs
+++ src/cmd/import.rs
@@ -7,6 +7,7 @@ use std::path::PathBuf;
 /// Import headers from backup or geth
 pub fn exec(path: String, to: u64, uri: Option<String>) -> Result<()> {
     std::env::set_var("RUST_LOG", "info,darwinia_shadow");
+    env_logger::init();
 
     // Build mmr client
     let mut client = build_client(&database(uri)?)?;

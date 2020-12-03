1b1460cf33de57c6dca6cd33933e3f7ae53923c4
Aki <wuminzhe@gmail.com>
2020-12-03 15:50:19 +0800

    Update import from geth's time elapsed output

--- mmr/src/mmr_client_trait.rs
+++ mmr/src/mmr_client_trait.rs
@@ -30,34 +30,29 @@ pub trait MmrClientTrait {
 
     fn import_from_geth(&mut self, geth_dir: PathBuf, til_block: u64) -> Result<()> {
         let start = Instant::now();
+
         let from = self.count()? as usize;
-        let elapsed = start.elapsed();
-        info!("count elapsed: {:?}", elapsed);
         if from >= til_block as usize {
             return Err(anyhow::anyhow!("The to position of mmr is {}, can not import mmr from {}, from must be less than to",
                 til_block, from
             ).into());
         }
 
-        let start = Instant::now();
         let hashes = ffi::import(geth_dir.to_str().unwrap(), from as i32, til_block as i32);
         let hashes_vec: Vec<&str> = hashes.split(',').collect::<Vec<&str>>();
-        let elapsed = start.elapsed();
-        info!("ffi::import elapsed: {:?}", elapsed);
 
-        let start = Instant::now();
         let size = hashes_vec.len();
         if !hashes.trim().is_empty() && size > 0 {
             info!("Importing ethereum headers from {:?}, size {} ...", geth_dir, size);
             self.batch_push(&hashes_vec)?;
             info!("Block {} ~ {}'s hash has been pushed into mmr store", from, from + size - 1);
             info!("Done.");
+
+            let elapsed = start.elapsed();
+            info!("batch_push elapsed: {:?}", elapsed);
         } else {
             info!("There is no block hash fetched between {} ~ {}", from, til_block);
         }
-        let elapsed = start.elapsed();
-        info!("batch_push elapsed: {:?}", elapsed);
-
 
         Ok(())
     }

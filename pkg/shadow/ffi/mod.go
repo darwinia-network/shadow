858ca40bcf197758f013f32de88efd07696c1e6e
xiaocheng <xiaoch2010@gmail.com>
2020-12-22 13:34:12 +0800

    rmv unused file

--- pkg/shadow/ffi/mod.go
+++ pkg/shadow/ffi/mod.go
@@ -46,18 +46,6 @@ func Proof(api string, number uint64) *C.char {
     return C.CString(eth.EncodeProofArray(proof.Format()))
 }
 
-//export Receipt
-func Receipt(api string, tx string) (*C.char, *C.char, *C.char) {
-    tx = "0x" + tx[2:]
-    proof, _, err := eth.GetReceipt(api, tx)
-    if err != nil {
-        log.Error("get receipt failed api %v, %v", api, err)
-        return C.CString(""), C.CString(""), C.CString("")
-    }
-
-    return C.CString(proof.Index), C.CString(proof.Proof), C.CString(proof.HeaderHash)
-}
-
 //export Import
 func Import(datadir string, from int, to int, batch int, callback unsafe.Pointer, arg unsafe.Pointer) bool {
     f := C.get_header(callback)

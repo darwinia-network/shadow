baa976a8a3b0da9ea088a09897ca3fea01df8572
Aki <wuminzhe@gmail.com>
2020-12-29 17:48:55 +0800

    Change 'leaf_index' to 'parent_leaf_index'

--- api/src/ethereum/parent_mmr_root.rs
+++ api/src/ethereum/parent_mmr_root.rs
@@ -26,12 +26,12 @@ pub async fn handle(block: Path<String>, app_data: Data<AppData>) -> impl Respon
 
 fn parent_mmr_root(block: Path<String>, mmr_db: &Database) -> Result<String> {
     let block: u64 = block.to_string().parse()?;
-    let leaf_index = block - 1;
+    let parent_leaf_index = block - 1;
     let client = build_client(mmr_db)?;
-    let result = client.get_mmr_root(leaf_index)?;
+    let result = client.get_mmr_root(parent_leaf_index)?;
     if let Some(root) = result {
         Ok(root)
     } else {
-        Err(Error::MmrRootNotFound(leaf_index))
+        Err(Error::MmrRootNotFound(parent_leaf_index))
     }
 }

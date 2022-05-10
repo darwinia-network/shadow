e5aeb73155dfd785160b0b373f75bcf68e033d7b
fewensa <fewensa@protonmail.com>
2022-05-10 21:21:30 +0800

    Try fix tests

--- api/tests/scale.rs
+++ api/tests/scale.rs
@@ -2,7 +2,8 @@ use core::num::flt2dec::decode;
 
 use codec::{Decode, Encode};
 use mock::{ha, header, proof, ETHASH_PROOF_CODEC, HEADER};
-use shadow_types::chain::ethereum::{EthashProof, EthereumHeader};
+use shadow_types::chain::ethereum::block::EthereumHeader;
+use shadow_types::chain::ethereum::ethash::EthashProof;
 
 mod mock;
 
@@ -34,22 +35,23 @@ fn mmr_proof() {
 #[test]
 fn eth_header() {
     let header = header();
-    let encoded = format!("0x{}", hex!(header.encode()));
+    let encoded = array_bytes::bytes2hex("0x", header.encode());
 
     assert_eq!(HEADER, encoded);
-    assert_eq!(
-        header,
-        EthereumHeader::decode(&mut bytes!(encoded.as_str()).as_ref()).unwrap()
-    );
+
+    let bytes = array_bytes::hex2bytes(&encoded).unwrap();
+    assert_eq!(header, EthereumHeader::decode(&mut bytes.as_ref()).unwrap());
 }
 
 #[test]
 fn decode_mmr_proof() {
-    <Vec<[u8; 32]>>::decode(&mut bytes!("0c04bd800035533b44381cb1ef207a3eb00c1e1cee6e561312b44c704a61624dd9e91bc2264d69287157d23d450e34f39925cebe653f3bf02d4a81a8308d02ad9d242553d4c882f9a6c72e34652b5245cc5ff0144037665a30695620ecdbe08c7f").as_ref()).unwrap();
+    let bytes = array_bytes::hex2bytes("0c04bd800035533b44381cb1ef207a3eb00c1e1cee6e561312b44c704a61624dd9e91bc2264d69287157d23d450e34f39925cebe653f3bf02d4a81a8308d02ad9d242553d4c882f9a6c72e34652b5245cc5ff0144037665a30695620ecdbe08c7f").unwrap();
+    <Vec<[u8; 32]>>::decode(&mut bytes.as_ref()).unwrap();
 }
 
 #[test]
 fn eth_hash_proof() {
-    let block = <Vec<EthashProof>>::decode(&mut bytes!(ETHASH_PROOF_CODEC).as_ref()).unwrap();
+    let bytes = array_bytes::hex2bytes(ETHASH_PROOF_CODEC).unwrap();
+    let block = <Vec<EthashProof>>::decode(&mut bytes.as_ref()).unwrap();
     assert_eq!(block[block.len() - 1], proof());
 }
